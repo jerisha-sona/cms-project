045c59dd37c2cfd4724682c5ef06bc5d7a71b37c
Kannika71 <uksrikannika@gmail.com>
2025-06-02 17:02:45 +0530

    payment path is changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
+import React from "react";
 import AppRoutes from "./routes/Approutes";
-
-
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
- 
-
   return (
-    <>
-       <AppRoutes />
-    </>    
-    
-  )
+    // AuthProvider wraps the entire app to provide authentication context
+    <AuthProvider>
+      {/* Define application routes */}
+      <AppRoutes />
+    </AuthProvider>
+  );
 }
+
 export default App;
